47d0b913c0d77b1a1a73d43d15920fad0456367e
Mate Marko <markomate90@gmail.com>
2020-11-19 14:06:57 +0100

    Eercises part of the they is done

---
+++ week-01/day-04/src/HelloOthers.java
@@ -0,0 +1,9 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Dani!");
+        System.out.println("Hello, Phuong!");
+        System.out.println("Hello, Andris!");
+
+    }
+
+}
