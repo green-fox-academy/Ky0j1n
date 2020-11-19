1b4ec6f272278b7e32f8542a8cd1db4f27699e8d
Mate Marko <markomate90@gmail.com>
2020-11-19 17:17:43 +0100

    Cuboid solution added

---
+++ week-01/day-04/src/Cuboid.java
@@ -0,0 +1,11 @@
+public class Cuboid {
+    public static void main(String[] args) {
+        double a = 11.3;
+        double b = 3;
+        double c = 12;
+        System.out.println("Surface Area: " + (6 * (a * a)));
+        System.out.println("Volume: " + (a*a*a));
+
+    }
+
+}
