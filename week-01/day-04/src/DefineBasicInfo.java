2923a868240bd14b867cf580cc2bd16225578e0c
Mate Marko <markomate90@gmail.com>
2020-11-19 17:07:12 +0100

    Added DefineBasic and Variable exercises solution

--- week-01/day-04/src/DefineBasicInfo.java
+++ week-01/day-04/src/DefineBasicInfo.java
@@ -1,2 +1,10 @@
 public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Mate Marko";
+        int age = 30;
+        double height = 1.98;
+        boolean married = false;
+
+    }
+
 }
