8859cfc714fa84dc8b44919ce08ac2d863f7bb7c
Mate Marko <markomate90@gmail.com>
2020-11-19 15:23:56 +0100

    Types exercise added

---
+++ week-01/day-04/src/CodingHours.java
@@ -0,0 +1,6 @@
+public class CodingHours {
+    public static void main(String[] args){
+        System.out.println("Spent with coding: " + ((17*7)-(17*2))*6);
+        System.out.println("Percentage of the coding hours: " + (int)(((((17.*7.)-(17.*2.))*6.)/(17.*52.))*100.) +"%");
+    }
+}
--- week-01/day-04/src/IntroduceYourself.java
+++ week-01/day-04/src/IntroduceYourself.java
@@ -1,6 +1,8 @@
 public class IntroduceYourself {
     public static void main(String[] args) {
-        
+        System.out.println("Mate");
+        System.out.println("30 years old");
+        System.out.println("1.96 meter");
     }
 
 }
