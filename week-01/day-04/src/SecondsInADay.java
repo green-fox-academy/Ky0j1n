02c238c784cebb507f09b9ac6801b9951d3baad8
Mate Marko <markomate90@gmail.com>
2020-11-19 17:31:49 +0100

    SecondsINADay has benn added

---
+++ week-01/day-04/src/SecondsInADay.java
@@ -0,0 +1,13 @@
+public class SecondsInADay {
+    public static void main(String[] args) {
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+        int leftOverHours = 24 - currentHours;
+        int leftOverMinutes = 60 - currentMinutes;
+        int leftOverSeconds = 60 - currentSeconds;
+        int secondsTillMidnight = (leftOverHours * 60 * 60) + (leftOverMinutes * 60) + leftOverSeconds;
+        System.out.println("Remaning seconds of the day: " + secondsTillMidnight);
+
+    }
+}
