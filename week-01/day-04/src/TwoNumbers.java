2daaa52eafdfb38cb8b32fc34b65cf72d2edfcf9
Mate Marko <markomate90@gmail.com>
2020-11-19 16:06:37 +0100

    Favorite number + Swap exercises done

--- week-01/day-04/src/TwoNumbers.java
+++ week-01/day-04/src/TwoNumbers.java
@@ -1,5 +1,5 @@
 public class TwoNumbers {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(13 + 2);
         System.out.println(13 - 22);
         System.out.println(22 * 13);
