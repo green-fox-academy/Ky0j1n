36d63d231167f0566c766139a2096dfcbc42d9df
Mate Marko <markomate90@gmail.com>
2020-11-20 23:28:00 +0100

    add DrawDiagonal

--- week-01/day-05/src/DrawDiagonal.java
+++ week-01/day-05/src/DrawDiagonal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // Write a program that reads a number from the standard input, then draws a
 // square like this:
 //
@@ -10,7 +12,20 @@
 //
 // The square should have as many lines as the number was
 public class DrawDiagonal {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        System.out.println("Insert a number:");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a; j++) {
+                if (i == 1 || j == 1 || j == a || i == j || i ==a ) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
