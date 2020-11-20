093015801d4b8941a5e943f84fe1087826738fe8
Mate Marko <markomate90@gmail.com>
2020-11-20 10:15:46 +0100

    Add AnimalAndLegs

--- week-01/day-05/src/AnimalsAndLegs.java
+++ week-01/day-05/src/AnimalsAndLegs.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
+
 // Write a program that asks for two integers
 // The first represents the number of chickens the farmer has
 // The second represents the number of pigs owned by the farmer
 // It should print how many legs all the animals have
 public class AnimalsAndLegs {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("How many chicen do you have my friend?");
         Scanner scanner = new Scanner(System.in);
         int chicken = scanner.nextInt();
         System.out.println("How many piggy do you have ?");
-        int piggy =  scanner.nextInt(System.in);
+        Scanner input = new Scanner(System.in);
+        int piggy = input.nextInt();
+        int legs = (chicken * 2) + (piggy * 4);
+        System.out.println("Your babies legs sum = " + legs);
     }
 }
