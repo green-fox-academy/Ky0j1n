5483c04d3126cd5fe8ac01760baf3104730bc5dc
Mate Marko <markomate90@gmail.com>
2020-11-21 23:54:52 +0100

    DrawChessTable added

--- week-01/day-05/src/DrawChessTable.java
+++ week-01/day-05/src/DrawChessTable.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 // Crate a program that draws a chess table like this
 //
@@ -13,14 +14,21 @@ import java.sql.SQLOutput;
 //
 public class DrawChessTable {
     public static void main(String[] args) {
-        int column = 5;
-        int row = 8;
-        for (int i = 1; i <= column; i++) {
-            for (int j = 1; j <= row; j++) {
-                if ((i % 2) != 0 && i< row) {
+        System.out.println("How many column would you like?");
+        Scanner scanner = new Scanner(System.in);
+        int column = scanner.nextInt();
+        System.out.println("How many row would you like?");
+        Scanner scanner2 = new Scanner(System.in);
+        int row = scanner2.nextInt();
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= column; j++) {
+                if ((i % 2 == 0) && (j == 1) || (i % 2 != 0) && (j == column)) {
+
+                    System.out.print(" ");
+                } else {
                     System.out.print("%");
                 }
-
             }
             System.out.println();
         }
