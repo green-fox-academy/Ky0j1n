12afed162918dbb90581f456c13f7fcb2ff347dc
Mate Marko <markomate90@gmail.com>
2020-11-22 22:24:24 +0100

    add feature to DrawPyramid (now u can insert how many row u would like to have in the pyramid

--- week-01/day-05/src/DrawPyramid.java
+++ week-01/day-05/src/DrawPyramid.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // Write a program that reads a number from the standard input, then draws a
 // pyramid like this:
 //
@@ -10,7 +12,9 @@
 // The pyramid should have as many lines as the number was
 public class DrawPyramid {
     public static void main(String[] args) {
-        int a = 4;
+        System.out.println("Please choose how many row should your Pyramid be!");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
         for (int i = 1; i <= a; i++) {
             for (int j = i; j < a; j++) {
                 System.out.print(" ");
