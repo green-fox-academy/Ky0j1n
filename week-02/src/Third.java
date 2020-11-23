69865fc47e1e421e6753df39b744182765dfed26
Mate Marko <markomate90@gmail.com>
2020-11-23 12:39:03 +0100

    add CompareLength

--- week-02/src/Third.java
+++ week-02/src/Third.java
@@ -2,7 +2,7 @@
 //   with the following content: `[4, 5, 6, 7]`
 // - Print the third element of `numbers`
 public class Third {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] numbers = {4, 5, 6, 7};
         System.out.println(numbers[2]);
     }
