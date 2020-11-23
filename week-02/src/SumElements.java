8a0db589ecaad26287b68068d37052677003015c
Mate Marko <markomate90@gmail.com>
2020-11-23 12:47:42 +0100

    add ChangeElements

--- week-02/src/SumElements.java
+++ week-02/src/SumElements.java
@@ -2,7 +2,7 @@
 //   with the following content: `[54, 23, 66, 12]`
 // - Print the sum of the second and the third element
 public class SumElements {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] numbers = {54, 23, 66, 12};
         System.out.println(numbers[1] + numbers[2]);
     }
