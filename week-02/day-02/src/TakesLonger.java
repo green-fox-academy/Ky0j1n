409a3c3a02d95679e81aeae83288c5466dcc296a
Mate Marko <markomate90@gmail.com>
2020-11-24 14:07:07 +0100

    add TakesLonger

---
+++ week-02/day-02/src/TakesLonger.java
@@ -0,0 +1,12 @@
+// When saving this quote a disk error has occurred. Please fix it.
+// Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+// Using pieces of the quote variable (instead of just redefining the string)
+public class TakesLonger {
+    public static void main(String[] args) {
+        String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+        String part1 = quote.substring(0, 20) + " always takes longer than";
+        String part2 = quote.substring(21);
+
+        System.out.println(part1 + part2);
+    }
+}
--- week-02/day-02/src/UrlFixer.java
+++ week-02/day-02/src/UrlFixer.java
@@ -2,17 +2,18 @@
 // Also, the URL is missing a crucial component, find out what it is and insert it too!
 // Try to solve it more than once using different String functions!
 public class UrlFixer {
-    public static String  fix(String input){
+    public static String fix(String input) {
         String get = input;
-        String firstPart = get.substring(0,5) + ":/";
+        String firstPart = get.substring(0, 5) + ":/";
         String secondPart = get.substring(6);
-        String together = firstPart + secondPart ;
+        String together = firstPart + secondPart;
         return together;
     }
- public static void  main(String[] args){
-     String url = "https//www.reddit.com/r/nevertellmethebots";
-     String url2 = url.replace("https//www.reddit.com/r/nevertellmethebots", "https//www.reddit.com/r/nevertellmetheodds")
-     String odds = url.substring(0, 38) + "odds";
-     System.out.println(fix(odds));
- }
+
+    public static void main(String[] args) {
+        String url = "https//www.reddit.com/r/nevertellmethebots";
+        String url2 = url.replace("https//www.reddit.com/r/nevertellmethebots", "https//www.reddit.com/r/nevertellmetheodds");
+        String odds = url.substring(0, 38) + "odds";
+        System.out.println(fix(odds));
+    }
 }
