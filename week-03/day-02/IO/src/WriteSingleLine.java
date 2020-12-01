a1692e1605310b2af7907b83565ff807f16d5fb1
Mate Marko <markomate90@gmail.com>
2020-12-01 15:45:21 +0100

    add WriteSingleLine

--- week-03/day-02/IO/src/WriteSingleLine.java
+++ week-03/day-02/IO/src/WriteSingleLine.java
@@ -1,4 +1,8 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // Write a function that is able to manipulate a file
@@ -6,12 +10,22 @@ import java.util.List;
 // The file should be named "my-file.txt"
 // In case the program is unable to write the file,
 // It should print the following error message: "Unable to write file: my-file.txt"
-//public class WriteSingleLine {
-//    public static String addName(String writte){
-//        List<String> content = new ArrayList();
-//
-//    }
-//    public static void main(String[] args){
-//
-//    }
-//}
+public class WriteSingleLine {
+    public static void addName(String addNaem) {
+        List<String> name = new ArrayList<String>();
+        name.add(addNaem);
+        try {
+            Path filePath = Paths.get("my-file.txt");
+            Files.write(filePath, name);
+        } catch (Exception e) {
+            System.out.println("Unable to write file: my-file.txt");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        String naem = "Mate";
+        addName(naem);
+    }
+}
