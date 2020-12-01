e0fc3340081095332ee63292b9a1d60d943f4a1a
Mate Marko <markomate90@gmail.com>
2020-12-01 14:07:47 +0100

    addPrintEachLine

--- week-03/day-02/IO/src/DivideByZero.java
+++ week-03/day-02/IO/src/DivideByZero.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 // and prints the result.
 // It should print "fail" if the parameter is 0
 public class DivideByZero {
-    public static Integer divideTen(int number){
+    public static Integer divideTen(int number) {
         int divided = 10 / number;
         return divided;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         System.out.println("How should I portion your 10?");
         Scanner scanner = new Scanner(System.in);
         int divideWith = scanner.nextInt();
         try {
-            System.out.println( divideTen(divideWith));
-        }catch(ArithmeticException e) {
+            System.out.println(divideTen(divideWith));
+        } catch (ArithmeticException e) {
             System.out.println("fail");
         }
 
