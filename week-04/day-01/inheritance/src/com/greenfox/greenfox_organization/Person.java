f02685bdf1a914f5173da57e28f00686076f432c
Mate Marko <markomate90@gmail.com>
2020-12-07 15:16:40 +0100

    add GreenFoxInheritenceExercise

---
+++ week-04/day-01/inheritance/src/com/greenfox/greenfox_organization/Person.java
@@ -0,0 +1,51 @@
+package com.greenfox.greenfox_organization;
+
+//Create a Person class with the following fields:
+//
+//        name: the name of the person
+//        age: the age of the person (whole number)
+//        gender: the gender of the person (male / female)
+//        And the following methods:
+//
+//        introduce(): prints out "Hi, I'm name, a age year old gender."
+//        getGoal(): prints out "My goal is: Live for the moment!"
+//        And the following constructors:
+//
+//        Person(name, age, gender)
+//        Person(): sets name to Jane Doe, age to 30, gender to female
+
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person() {
+        name = "Jane Doe";
+        age = 30;
+        gender = "female";
+    }
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String commonIntroduce() {
+        return "Hi, I'm " + name + ", a " + age + " year old " + gender;
+    }
+
+    public void introduce() {
+        System.out.println(commonIntroduce() + ".");
+    }
+
+    public String commonGetGoal() {
+        return "My goal is: ";
+    }
+
+    public void getGoal() {
+        System.out.println(commonGetGoal() + "Live for the moment!");
+    }
+
+
+}
