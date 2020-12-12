813a803c647658a0a5f83d5cc40fd5e34bd8e510
Mate Marko <markomate90@gmail.com>
2020-12-12 18:32:56 +0100

    revised week3 day2 PrintEachLine

--- Practice/src/practice.java
+++ Practice/src/practice.java
@@ -1,34 +1,99 @@
-////  Create a function that takes an array of numbers as a parameter
-////  and returns an array of integers where every integer is unique (occurs only once)
-//
-////  Example
-////        System.out.println(findUniqueItems(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
-////  should print: `[1, 11, 34, 52, 61]`
+// Write a program that opens a file called "my-file.txt", then prints
+// each line from the file.
+// If the program is unable to read the file (for example it does not exist),
+// then it should print the following error message: "Unable to read file: my-file.txt"
+
+
+// Write a function that takes a filename as string,
+// Write a function that is able to manipulate a file
+// By writing your name into it as a single line
+// The file should be named "my-file.txt"
+// In case the program is unable to write the file,
+// It should print the following error message: "Unable to write file: my-file.txt"
+
+import javax.swing.*;
+import java.io.IOException;
+
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class practice {
-//
-//    public static void main(String[] args) {
-//      int[] numbers = {1, 11, 34, 11, 52, 61, 1, 34, 34};
-//    }
-//    public int[] uniqueNumbers(int[] array){
-//        int[] result = new int[array.length]
-//
-//    }
-//}
-public static void main(String[] args){
-    int gizi= dia(1,2);
-    System.out.println(gizi);
+    public static void main(String[] args) {
+        practice a = new practice();
+            a.addString("Mate");
+            a.addToAnExictentFile("Aoi");
+            a.addToAnExictentFile("Hanako");
+            a.getFileContent();
+
+    }
+
+    public void addToAnExictentFile(String lineToAdd){
+            List<String> content = new ArrayList<>();
+            List<String> contentOfTheNewFile = new ArrayList<>();
+            content.addAll(fileContent());
+            content.add(lineToAdd);
+
+        try {
+            Files.write(pathToTheUsedFile(),content);
+        } catch (IOException e) {
+            System.out.println("unable to writte file");
+        }
 
 
+    }
+    public Path pathToTheUsedFile(){
+        Path filePath = Paths.get("src/my-file.txt");
+        return filePath;
+    }
+    public void addString(String writeALine){
+        List<String> content = new ArrayList<>();
+        content.add(writeALine);
+            Path filePath = Paths.get("src/my-file.txt");
+        try {
+            Files.write(filePath,content);
+        } catch (IOException e) {
+            System.out.println("no file!");
+        }
 
 
+    }
 
-    gizi = 13;
-    System.out.println(gizi);
+    public List <String> fileContent(){
+        Path filePath = Paths.get("src/my-file.txt");
+        List<String> content = new ArrayList<>();
+        try {
+            content.addAll(Files.readAllLines(filePath));
+        } catch (IOException e) {
+            System.out.println("No, file!");
+        }
+        return content;
+    }
+
+    public void  getFileContent(){
+        int order = 1;
+        Path filePath =  Paths.get("src/my-file.txt");
+        List<String> content = new ArrayList<>();
+        try {
+            content.addAll(Files.readAllLines(filePath));
+        } catch (IOException e) {
+            System.out.println("can't reach the file");
+            System.exit(-1);
+        }
+        for(String line : content){
+            System.out.println(order + " $ " + line);
+            order++;
+        }
+
+    }
+
+
 
 }
 
- public static int dia(int n1, int n2){
-        int result = n1 + n2;
-        return result;
- }
-}
+
+
+
--- week-03/day-02/IO/src/PrintEachLine.java
+++ week-03/day-02/IO/src/PrintEachLine.java
@@ -2,6 +2,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.rmi.server.ExportException;
+import java.util.ArrayList;
 import java.util.List;
 
 // Write a program that opens a file called "my-file.txt", then prints
@@ -10,11 +11,15 @@ import java.util.List;
 // then it should print the following error message: "Unable to read file: my-file.txt"
 public class PrintEachLine {
     public static void main(String[] args) {
+                List<String> content = new ArrayList<>();
+            Path filePath = Paths.get("src/my-file");
         try {
-            Path filePath = Paths.get("my-file.txt");
-            List<String> lines = Files.readAllLines(filePath);
+           content = Files.readAllLines(filePath);
         } catch (Exception e) {
             System.out.println("Unable to read file: my-file.txt");
+        }
+        for(String line: content){
+            System.out.println(line);
         }
     }
 }
