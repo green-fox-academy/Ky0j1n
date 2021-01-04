c9356aff9905f4f5fb87b2e11d00b53d7be5ad0f
Mate Marko <markomate90@gmail.com>
2021-01-04 11:47:37 +0100

    fixed stream format at 1th exercise, + add 2nd exercise

--- week -06/day-01/src/com/greenfox/exercise2/Main.java
+++ week -06/day-01/src/com/greenfox/exercise2/Main.java
@@ -1,7 +1,5 @@
-package com.greenfox.exercise1;
-//Exercise 1
-//        Write a Stream Expression to get the even numbers from the following list:
-//
+package com.greenfox.exercise2;
+//Write a Stream Expression to get the squared value of the positive numbers from the following list:
 //        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
 
 import java.util.Arrays;
@@ -10,9 +8,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
-
         numbers.stream()
-                .filter(even -> even % 2 == 0 )
+                .filter(positiveNumber -> positiveNumber > 0)
+                .map(number -> number * number)
                 .forEach(System.out::println);
     }
 }
