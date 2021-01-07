5afc61c8d0aa75413aa39085dc36da5d22da8cc7
Mate Marko <markomate90@gmail.com>
2021-01-07 08:20:14 +0100

    add Hello User REST App

--- week -06/day-03/src/main/java/com/greenfoxacademy/springstart/helloworld/Main.java
+++ week -06/day-03/src/main/java/com/greenfoxacademy/springstart/helloworld/Main.java
@@ -1,4 +1,4 @@
-package com.greenfoxacademy.springstart.HelloWorld;
+package com.greenfoxacademy.springstart.helloworld;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- week -06/day-03/src/main/java/com/greenfoxacademy/springstart/helloworldrestapp/controllers/HelloRESTController.java
+++ week -06/day-03/src/main/java/com/greenfoxacademy/springstart/helloworldrestapp/controllers/HelloRESTController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.springstart.helloworldrestapp.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,8 @@ public class HelloRESTController {
 
     @RequestMapping(value="greeting")
     @ResponseBody
-    public Greeting greeting() {
-        Greeting greeting = new Greeting(1, "Minden jó!!!");
+    public Greeting greeting(@RequestParam String name) {
+        Greeting greeting = new Greeting(1, "Minden jó, " + name + " !!!");
         return greeting;
     }
 
