4f5d3b5c810d2b0ea0e3af7f293367493716daa2
Mate Marko <markomate90@gmail.com>
2021-01-07 16:06:10 +0100

    add HTMLception

--- week -06/day-04/bank/src/main/java/com/greenfox/bankofsimba/demo/Controller/BankAccountController.java
+++ week -06/day-04/bank/src/main/java/com/greenfox/bankofsimba/demo/Controller/BankAccountController.java
@@ -3,6 +3,7 @@ package com.greenfox.bankofsimba.demo.Controller;
 import com.greenfox.bankofsimba.demo.model.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,5 +26,12 @@ public class BankAccountController {
     public String showAccount(Model model) {
        model.addAttribute("bankAccountList" , bankAccountList);
        return "index";
+    }
+
+    @RequestMapping ( path = "/text", method = RequestMethod.GET)
+    public String text(Model model) {
+       String text= "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
+       model.addAttribute("text", text);
+       return "text";
     }
 }
