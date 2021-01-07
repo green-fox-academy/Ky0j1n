f7fe5ae7960e7ba80f603e06d6962be41902b506
Mate Marko <markomate90@gmail.com>
2021-01-07 08:03:33 +0100

    add Hello World REST App

---
+++ week -06/day-03/src/main/java/com/greenfoxacademy/springstart/helloworldrestapp/controllers/HelloRESTController.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy.springstart.helloworldrestapp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRESTController {
+
+    @RequestMapping(value="greeting")
+    @ResponseBody
+    public Greeting greeting() {
+        Greeting greeting = new Greeting(1, "Minden jó!!!");
+        return greeting;
+    }
+
+}
